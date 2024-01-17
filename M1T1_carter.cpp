a0b7fd0039d8354f22eade9fe5f86c3c0fae7cee
Riplxck <156948040+Riplxck@users.noreply.github.com>
2024-01-17 19:37:27 +0000

    M1T1 done

---
+++ M1T1_carter.cpp
@@ -0,0 +1,19 @@
+/*
+CSC 134
+M1T1- Hello world 
+L Carter
+1/17/24
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+    cout << "Hello World" << endl;
+    string color = "Green";
+    cout << "my favorite color is " << color << endl;
+cout << "whats yours?" ;
+string answer;
+cin >> answer;
+cout << "you said " << answer << endl;
+    return 0;
+}
