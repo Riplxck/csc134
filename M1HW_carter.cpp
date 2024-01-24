599caeea9ecc2d07e513ff862723e34865f0beea
Riplxck <156948040+Riplxck@users.noreply.github.com>
2024-01-24 19:36:39 +0000

    still need to add the rest

---
+++ M1HW_carter.cpp
@@ -0,0 +1,23 @@
+/*
+CSC 134 
+M1HW- movie
+L carter
+1/24/24
+*/
+#include <iostream>
+using namespace std;
+
+int main () {
+// something to hold name of movie
+// something to hold the year that it came out 
+// something to hold the box office gross
+// give brief summary of the movie nightswim using 4 to 5 sentences and using a few quotes
+//set up variables
+string movie_name = "Night swim";
+int release_date = 2024;
+double office_gross = 37.2;
+cout << "The movie "<< movie_name << endl;
+cout << endl;
+
+    return 0;
+}
