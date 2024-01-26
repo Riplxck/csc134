d1bd99efa4d401e24e8a6d73bcdb5ab06ab15d6d
Riplxck <156948040+Riplxck@users.noreply.github.com>
2024-01-26 02:48:01 +0000

    finished M1HW

--- M1HW_carter.cpp
+++ M1HW_carter.cpp
@@ -16,7 +16,15 @@ int main () {
 string movie_name = "Night swim";
 int release_date = 2024;
 double office_gross = 37.2;
-cout << "The movie "<< movie_name << endl;
+string dollar_amount = " million dollars.";
+cout << "The movie "<< movie_name << " came out in January of 2024 " << "and had a worldwide office gross of " << office_gross << dollar_amount << endl;
+cout << "The story took place at a home" << " of a family that were unaware" << " of the houses darkest secret." << endl;
+cout << "A girl was murdered in the swimming pool in the houses backyard" << " which actuated the river of sacrifices" << " causing the swimming pools ealier deaths to murder anything in it." << endl;
+cout << "The story ends by the dad" << " sacrificing his life to save his son" << " which earlier in the scene got tooken but came back to life" << "after the dad sacrificed his self." << endl;
+
+
+cout << "quote" << endl;
+cout << " \"Sometimes the darkest nights lead to the brightest mornings\" " << endl;
 cout << endl;
 
     return 0;
