ee71fb5824204d6e9c6714a91961ba61131975ad
Riplxck <156948040+Riplxck@users.noreply.github.com>
2024-03-17 16:32:35 +0000

    finished M4HW1s2

---
+++ m4/M4HW1s2_carter.cpp
@@ -0,0 +1,27 @@
+/*
+CSC 134 
+M4HW1-silver1
+L carter
+3/17/24
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // declare variables
+    int firstNum, secondNum, answer;
+    firstNum = 0;
+    // firstNum = 5;
+     // secondNum = 1;
+     answer = firstNum * secondNum;
+    cout << "Enter a number from 1-12" << endl;
+     cin >> firstNum;
+for(int i=1; i<= 12; i++) {
+    answer = firstNum * i;
+    cout << firstNum << " times " << i << " is " << answer << endl;
+}
+
+
+    return 0;
+}
